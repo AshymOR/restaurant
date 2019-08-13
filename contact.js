e82fdaf770f50d821e438a2e81efe15266e73052
Don <lordashym@gmail.com>
2019-08-13 13:09:47 -0500

    done

--- contact.js
+++ contact.js
@@ -1,8 +1,18 @@
 function validateForm() {
     var x = document.forms["myForm"]["name"].value;
+    var y = document.forms["myForm"]["email"].value;
+    var z = document.forms["myForm"]["phone"].value;
     if (x == "") {
       alert("Name must be filled out");
       return false;
+    }
+    else if (y == "") {
+        alert("Email must be filled out");
+        return false;
+    }
+    else if (z == "") {
+        alert("Phone must be filled out");
+        return false;
     }
   }
   
